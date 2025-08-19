094e5f89b165513981a5035745ae0d0298670ab1
ClaudioMC <claudiomacouto@gmail.com>
2025-08-18 22:18:42 -0400

    melhorias e geoloc

--- js/script.js
+++ js/script.js
@@ -11,19 +11,20 @@ const umidityElement = document.querySelector("#umidade span");
 const windElement = document.querySelector("#vento span");
 
 const getDadosClima = async (cidade) => {
-
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`;
-
     const res = await fetch(apiUrl);
-    const data = await res.json()
-
+    const data = await res.json();
     return data;
+};
 
-}
+const getDadosClimaCoords = async (lat, lon) => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    return data;
+};
 
-const mostraClima = async (cidade) => {
-    const data = await getDadosClima(cidade);
-
+function atualizarUI(data) {
     cityElement.innerText = data.name;
     tempElement.innerText = parseInt(data.main.temp);
     descElement.innerText = data.weather[0].description;
@@ -33,16 +34,35 @@ const mostraClima = async (cidade) => {
     );
     umidityElement.innerText = `${data.main.humidity}%`;
     windElement.innerText = `${data.wind.speed} km/h`;
+}
+
+const mostraClima = async () => {
+    const cidade = inputCidade.value.trim();
+
+    if (cidade) {
+        const data = await getDadosClima(cidade);
+        atualizarUI(data);
+    } else {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                async (pos) => {
+                    const lat = pos.coords.latitude;
+                    const lon = pos.coords.longitude;
+                    const data = await getDadosClimaCoords(lat, lon);
+                    atualizarUI(data);
+                },
+                (err) => {
+                    alert("Não foi possível obter a localização.");
+                    console.error(err);
+                }
+            );
+        } else {
+            alert("Geolocalização não é suportada neste navegador.");
+        }
+    }
 };
-
-
 
 pesquisaBtn.addEventListener("click", (e) => {
     e.preventDefault();
-
-    const cidade = inputCidade.value;
-
-    if(cidade){
-        mostraClima(cidade);
-    }
+    mostraClima();
 });
